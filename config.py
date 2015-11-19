e2e4bba5384132459ce95dfe79b61ae59073fde0
PandiSelvi <selvi.rajangam@gmail.com>
2015-11-19 15:49:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
 ######Tenant and users####
-tenant_name= "demo"
-user_name= "demo"
-password= "password"
+tenant_name = "demo"
+user_name = "demo"
+password = "password"
 
 ######Volume######
-non_bootable_volume_name="non_boot_vol"
-non_bootable_volume_size= 1
-bootable_volume_name="boot_vol"
-bootable_volume_size=3
-bootable_volume_size_for_data=5
-volume_block_size=2000
-count=1000
-volume_limit=10
-image_name="cirros-0.3.4-x86_64-uec"
+non_bootable_volume_name = "non_boot_vol"
+non_bootable_volume_size = 1
+bootable_volume_name = "boot_vol"
+bootable_volume_size = 3
+bootable_volume_size_for_data = 5
+volume_block_size = 2000
+count = 1000
+volume_limit= 10
+image_name = "cirros-0.3.4-x86_64-uec"
 
 ######Instance#####
-flavor=2
-key_name="oskey1"
-network="private"
-security_group="default"
-instance_name="boot_inst"
+flavor = 2
+key_name = "oskey1"
+network = "private"
+security_group = "default"
+instance_name = "boot_inst"
 
 #######Backup#####
-backup_name="back_up"
-backup_limit=10
+backup_name = "back_up"
+backup_limit = 10
 
 
 
